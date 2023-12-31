904237f6dce63c5ec52064fb2e949edb07ab8612
sofiane <idri.sof3@gmail.com>
2023-12-31 16:59:34 -0500

    parcours

--- src/app/component/parcours/parcours.component.ts
+++ src/app/component/parcours/parcours.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {HeadComponent} from "../head/head.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-parcours',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    HeadComponent,
+    FooterComponent
   ],
   templateUrl: './parcours.component.html',
   styleUrl: './parcours.component.scss'
