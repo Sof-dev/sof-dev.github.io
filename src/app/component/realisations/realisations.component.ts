4830894f9525c790e1e8a3dae527669e2acfa9cf
sofiane <idri.sof3@gmail.com>
2024-01-03 17:28:43 -0500

    realisations

--- src/app/component/realisations/realisations.component.ts
+++ src/app/component/realisations/realisations.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {FooterComponent} from "../footer/footer.component";
+import {HeadComponent} from "../head/head.component";
 
 @Component({
   selector: 'app-realisations',
   standalone: true,
-  imports: [
-    HeaderComponent
-  ],
+    imports: [
+        HeaderComponent,
+        FooterComponent,
+        HeadComponent
+    ],
   templateUrl: './realisations.component.html',
   styleUrl: './realisations.component.scss'
 })
